5b35430fb827b2241e3e6c611315a43527c1ef3f
Scott Burnett <G14B4098@ict.ru.ac.za>
2019-02-21 15:32:51 +0200

    Problems.Tests created, Problems\Program.cs modified

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace Problems
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Problems{
+	
+    public class Program{
+		
+        static void Main(string[] args){
+			Program p = new Program();
+            Console.WriteLine(p.Prefix("Hello World!"));
         }
+		
+		public string Prefix(string input){
+			if (input == null)
+				return null;
+			int words = 0 , Length = input.Length;
+			for ( int i = 0; i < Length; i++)
+				if (input[i] == ' ')
+					words++;
+			if (Length > 1) 
+				words ++;
+			string result = Length + "," + words + ":" + input;
+			return result;
+		}
     }
 }
