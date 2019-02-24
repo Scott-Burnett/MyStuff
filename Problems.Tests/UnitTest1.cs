55600856725f776308927f164b6a90902a6668e2
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2019-02-24 09:17:07 +0200

    Cleaning up. Last 2 points not completed

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -9,26 +9,41 @@ namespace Tests
         public void Setup()
         {
         }
-
+        
         [Test]
         public void Test1(){
+            //Arrange
 			Program p = new Program();
+
+            //Act
             string result = p.Prefix("hello");
+
+            //Assert
 			Assert.AreEqual(result, "5,1:hello" );
         }
 		
 		[Test]
 		public void Test2(){
+            //Arrange
 			Program p = new Program();
-			string result = p.Prefix("");
-			Assert.AreEqual(result, "0,0:");
+
+            //Act
+            string result = p.Prefix("");
+
+            //Assert
+            Assert.AreEqual(result, "0,0:");
 		}
 		
 		[Test]
 		public void Test3(){
+            //Arrange
 			Program p = new Program();
-			string result = p.Prefix("what  ...  did  you  say?? ");
-			Assert.AreEqual(result, "27,5:what  ...  did  you  say?? ");
+
+            //Act
+            string result = p.Prefix("what  ...  did  you  say?? ");
+
+            //Assert
+            Assert.AreEqual(result, "27,5:what  ...  did  you  say?? ");
 		}
     }
 }
